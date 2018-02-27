fb202203cf9fe305b54192332e50be2d8c695af5
jhandfield <justin@handfield.org>
2018-02-26 19:53:55 -0500

    Built out ResetData class then realized I haven't finished object loading yet...

--- ROMSharp/ROMSharp/Models/ResetData.cs
+++ ROMSharp/ROMSharp/Models/ResetData.cs
@@ -3,8 +3,43 @@ namespace ROMSharp.Models
 {
     public class ResetData
     {
-        public ResetData()
-        {
-        }
+        #region Properties
+        /// <summary>
+        /// Determines the command the reset should perform
+        /// </summary>
+        public ResetCommand Command { get; set; }
+
+        /// <summary>
+        /// First argument for the reset - varies by reset type
+        /// </summary>
+        public int Arg1 { get; set; }
+
+        /// <summary>
+        /// Second argument for the reset - varies by reset type
+        /// </summary>
+        public int Arg2 { get; set; }
+
+        /// <summary>
+        /// Third argument for the reset - varies by reset type
+        /// </summary>
+        public int Arg3 { get; set; }
+
+        /// <summary>
+        /// Fourth argument for the reset - varies by reset type
+        /// </summary>
+        public int Arg4 { get; set; }
+        #endregion
+
+        public ResetData() { }
+    }
+
+    public enum ResetCommand {
+        SpawnMobile = 'M',
+        SpawnObject = 'O',
+        PutInObject = 'P',
+        GiveObjectToMob = 'G',
+        EquipObjectOnMob = 'E',
+        SetDoorState = 'D',
+        RandomizeExits = 'R'
     }
 }
