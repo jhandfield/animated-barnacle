923ed3386c2c9f5488f1da53c6453f3d2867c927
Justin Handfield <justin@handfield.org>
2018-03-14 20:33:06 -0400

    Forgot to include the unit test project...

---
+++ ROMSharp/UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests.cs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTests.cs")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("82255066-241d-498c-9d7b-404dec7778a7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ROMSharp/UnitTests/RoomTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using ROMSharp.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class RoomTests
+    {
+        [TestMethod]
+        public void TestValidRoomData()
+        {
+            // Room data to parse, taken from daycare.are
+            string roomData = @"#6601
+The Dwarven Drop Off~
+This is where the dwarven mommies and daddies come to drop off their
+annoying little brats.  You have the urge to leave.  There is a sign here.
+~
+0 CD 0
+D0
+~
+~
+0 -1 6500
+D2
+You see a large room full of noisy brats.
+~
+~
+1 -1 6602
+E
+sign~
+This is an area for newbies, so have fun.  -- Sandman
+~
+S";
+            StringReader sr = new StringReader(roomData);
+            int lineNum = 1;
+            string firstLine = sr.ReadLine();
+            string expectedName = "The Dwarven Drop Off";
+            string southExitDesc = "You see a large room full of noisy brats.";
+            int southExitVNUM = 6602;
+
+            try
+            {
+                RoomIndexData room = RoomIndexData.ParseRoomData(ref sr, "testArea", ref lineNum, firstLine);
+
+                // Name of the room should be "The Dwarven Drop Off"
+                Assert.AreEqual(expectedName, room.Name, String.Format("Room name mismatch, expected \"{0}\" but found \"{1}\"", expectedName, room.Name));
+
+                // There should be an exit to the south
+                Assert.IsNotNull(room.Exits[2], "Expected a south exit, but found none");
+
+                // Check south exit's description
+                Assert.AreEqual(southExitDesc, room.Exits[2].Description);
+
+                // There should not be an exit to the east
+                Assert.IsNull(room.Exits[1], "Expected no east exit, but found one");
+
+                // Expect the room flags to indicate indoors and no mobs (CD)
+                Assert.AreEqual(RoomAttributes.Indoors | RoomAttributes.NoMobs, room.Attributes);
+
+                // Expect the south exit to lead to rom 6602
+                Assert.AreEqual(southExitVNUM, room.Exits[2].ToVNUM, String.Format("Expected south exit to lead to room {0}, but actually exits to {1}", southExitVNUM, room.Exits[2].ToVNUM));
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(String.Format("Execption thrown loading room: {0} {1}", e.GetType().ToString(), e.Message));
+            }
+        }
+    }
+}
