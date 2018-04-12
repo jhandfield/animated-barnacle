4d533ca0bcae4e35b3e48e8838330e3b41be9856
jhandfield <justin@handfield.org>
2018-04-12 09:24:13 -0400

    Updated ItemExtraFlag and WearFlag to the new declaration format

--- ROMSharp/ROMSharp/Enums/ItemExtraFlag.cs
+++ ROMSharp/ROMSharp/Enums/ItemExtraFlag.cs
@@ -4,29 +4,125 @@ namespace ROMSharp.Enums
     public enum ItemExtraFlag
     {
         None = 0,
-        Glow = 1 << 0,
-        Hum = 1 << 1,
-        Dark = 1 << 2,
-        Lock = 1 << 3,
-        Evil = 1 << 4,
-        Invis = 1 << 5,
-        Magic = 1 << 6,
-        NoDrop = 1 << 7,
-        Bless = 1 << 8,
-        AntiGood = 1 << 9,
-        AntiEvil = 1 << 10,
-        AntiNeutral = 1 << 11,
-        NoRemove = 1 << 12,
-        Inventory = 1 << 13,
-        NoPurge = 1 << 14,
-        RotDeath = 1 << 15,
-        VisDeath = 1 << 16,
-        NonMetal = 1 << 18,
-        NoLocate = 1 << 19,
-        MeltDrop = 1 << 20,
-        HadTimer = 1 << 21,
-        SellExtract = 1 << 22,
-        BurnProof = 1 << 23,
-        NoUnCurse = 1 << 25
+
+        /// <summary>
+        /// Legacy ITEM_GLOW (A)
+        /// </summary>
+        Glow = AlphaMacros.A,
+
+        /// <summary>
+        /// Legacy ITEM_HUM (B)
+        /// </summary>
+        Hum = AlphaMacros.B,
+
+        /// <summary>
+        /// Legacy ITEM_DARK (C)
+        /// </summary>
+        Dark = AlphaMacros.C,
+
+        /// <summary>
+        /// Legacy ITEM_LOCK (D)
+        /// </summary>
+        Lock = AlphaMacros.D,
+
+        /// <summary>
+        /// Legacy ITEM_EVIL (E)
+        /// </summary>
+        Evil = AlphaMacros.E,
+
+        /// <summary>
+        /// Legacy ITEM_INVIS (F)
+        /// </summary>
+        Invis = AlphaMacros.F,
+
+        /// <summary>
+        /// Legacy ITEM_MAGIC (G)
+        /// </summary>
+        Magic = AlphaMacros.G,
+
+        /// <summary>
+        /// Legacy ITEM_NODROP (H)
+        /// </summary>
+        NoDrop = AlphaMacros.H,
+
+        /// <summary>
+        /// Legacy ITEM_BLESS (I)
+        /// </summary>
+        Bless = AlphaMacros.I,
+
+        /// <summary>
+        /// Legacy ITEM_ANTI_GOOD (J)
+        /// </summary>
+        AntiGood = AlphaMacros.J,
+
+        /// <summary>
+        /// Legacy ITEM_ANTI_EVIL (K)
+        /// </summary>
+        AntiEvil = AlphaMacros.K,
+
+        /// <summary>
+        /// Legacy ITEM_ANTI_NEUTRAL (L)
+        /// </summary>
+        AntiNeutral = AlphaMacros.L,
+
+        /// <summary>
+        /// Legacy ITEM_NOREMOVE (M)
+        /// </summary>
+        NoRemove = AlphaMacros.M,
+
+        /// <summary>
+        /// Legacy ITEM_INVENTORY (N)
+        /// </summary>
+        Inventory = AlphaMacros.N,
+
+        /// <summary>
+        /// Legacy ITEM_NOPURGE (O)
+        /// </summary>
+        NoPurge = AlphaMacros.O,
+
+        /// <summary>
+        /// Legacy ITEM_ROT_DEATH (P)
+        /// </summary>
+        RotDeath = AlphaMacros.P,
+
+        /// <summary>
+        /// Legacy ITEM_VIS_DEATH (Q)
+        /// </summary>
+        VisDeath = AlphaMacros.Q,
+
+        /// <summary>
+        /// Legacy ITEM_NONMETAL (S)
+        /// </summary>
+        NonMetal = AlphaMacros.S,
+
+        /// <summary>
+        /// Legacy ITEM_NOLOCATE (T)
+        /// </summary>
+        NoLocate = AlphaMacros.T,
+
+        /// <summary>
+        /// Legacy ITEM_MELT_DROP (U)
+        /// </summary>
+        MeltDrop = AlphaMacros.U,
+
+        /// <summary>
+        /// Legacy ITEM_HAD_TIMER (V)
+        /// </summary>
+        HadTimer = AlphaMacros.V,
+
+        /// <summary>
+        /// Legacy ITEM_SELL_EXTRACT (W)
+        /// </summary>
+        SellExtract = AlphaMacros.W,
+
+        /// <summary>
+        /// Legacy ITEM_BURN_PROOF (Y)
+        /// </summary>
+        BurnProof = AlphaMacros.Y,
+
+        /// <summary>
+        /// Legacy ITEM_NOUNCURSE (Z)
+        /// </summary>
+        NoUnCurse = AlphaMacros.Z
     }
 }
--- ROMSharp/ROMSharp/Enums/WearFlag.cs
+++ ROMSharp/ROMSharp/Enums/WearFlag.cs
@@ -4,22 +4,90 @@ namespace ROMSharp.Enums
     public enum WearFlag
     {
         None = 0,
-        Take = 1 << 0,
-        Wear_Finger = 1 << 1,
-        Wear_Neck = 1 << 2,
-        Wear_Body = 1 << 3,
-        Wear_Head = 1 << 4,
-        Wear_Legs = 1 << 5,
-        Wear_Feet = 1 << 6,
-        Wear_Hands = 1 << 7,
-        Wear_Arms = 1 << 8,
-        Wear_Shield = 1 << 9,
-        Wear_About = 1 << 10,
-        Wear_Waist = 1 << 11,
-        Wear_Wrist = 1 << 12,
-        Wield = 1 << 13,
-        Hold = 1 << 14,
-        NoSacrifice = 1 << 15,
-        Wear_Float = 1 << 16
+
+        /// <summary>
+        /// Legacy ITEM_TAKE (A)
+        /// </summary>
+        Take = AlphaMacros.A,
+
+        /// <summary>
+        /// Legacy ITEM_WEAR_FINGER (B)
+        /// </summary>
+        Wear_Finger = AlphaMacros.B,
+
+        /// <summary>
+        /// Legacy ITEM_WEAR_NECK (C)
+        /// </summary>
+        Wear_Neck = AlphaMacros.C,
+
+        /// <summary>
+        /// Legacy ITEM_WEAR_BODY (D)
+        /// </summary>
+        Wear_Body = AlphaMacros.D,
+
+        /// <summary>
+        /// Legacy ITEM_WEAR_HEAD (E)
+        /// </summary>
+        Wear_Head = AlphaMacros.E,
+
+        /// <summary>
+        /// Legacy ITEM_WEAR_LEGS (F)
+        /// </summary>
+        Wear_Legs = AlphaMacros.F,
+
+        /// <summary>
+        /// Legacy ITEM_WEAR_FEET (G)
+        /// </summary>
+        Wear_Feet = AlphaMacros.G,
+
+        /// <summary>
+        /// Legacy ITEM_WEAR_HANDS (H)
+        /// </summary>
+        Wear_Hands = AlphaMacros.H,
+
+        /// <summary>
+        /// Legacy ITEM_WEAR_ARMS (I)
+        /// </summary>
+        Wear_Arms = AlphaMacros.I,
+
+        /// <summary>
+        /// Legacy ITEM_WEAR_SHIELD (J)
+        /// </summary>
+        Wear_Shield = AlphaMacros.J,
+
+        /// <summary>
+        /// Legacy ITEM_WEAR_ABOUT (K)
+        /// </summary>
+        Wear_About = AlphaMacros.K,
+
+        /// <summary>
+        /// Legacy ITEM_WEAR_WAIST (L)
+        /// </summary>
+        Wear_Waist = AlphaMacros.L,
+
+        /// <summary>
+        /// Legacy ITEM_WEAR_WRIST (M)
+        /// </summary>
+        Wear_Wrist = AlphaMacros.M,
+
+        /// <summary>
+        /// Legacy ITEM_WIELD (N)
+        /// </summary>
+        Wield = AlphaMacros.N,
+
+        /// <summary>
+        /// Legacy ITEM_HOLD (O)
+        /// </summary>
+        Hold = AlphaMacros.O,
+
+        /// <summary>
+        /// Legacy ITEM_NO_SAC (P)
+        /// </summary>
+        NoSacrifice = AlphaMacros.P,
+
+        /// <summary>
+        /// Legacy ITEM_WEAR_FLOAT (Q)
+        /// </summary>
+        Wear_Float = AlphaMacros.Q
     }
 }
